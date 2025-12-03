70eda71fe46ba06f88873f9850b83e3e259397e8
tornikejan <51330864+tornikejanashvili@users.noreply.github.com>
2025-12-03 17:28:49 +0400

    Lecture 12 - javascript
    javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+const userName = "John" ;
+const userSurname = "Doe";
+let userAge = "30";
+const userMail = "johndoe@userMail.com";
+const userMobilenumber = "577898887";
+let isadult = true;
+
+
+
+console.log(userName)
+console.log(userSurname)
+console.log(userAge)
+console.log(userMail)
+console.log("%cuserMobilenumber","color:red", userMobilenumber)
+console.log("%cisadult", "color: yellow;", isadult)
+
+// ისე ვერ გავაფერადე რომ ცვლადის შინაარსი ფერით გამოსულიყო //
+
+
